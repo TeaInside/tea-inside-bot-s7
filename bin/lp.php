7089f8df492f0c0a3042a69db407ab75524d4948
Ammar Faizi <ammarfaizi2@gmail.com>
2020-01-24 17:23:09 +0700

    Clean up

--- bin/lp.php
+++ bin/lp.php
@@ -2,8 +2,20 @@
 
 $php = "<?php ".trim(substr(file_get_contents(__FILE__), __COMPILER_HALT_OFFSET__));
 
-echo shell_exec(
-    "/bin/echo ".escapeshellarg($php)." | php -d extension=".escapeshellarg(__DIR__."/../icetea_bot.so"));
+$proc = proc_open(
+	"php -d extension=".escapeshellarg(__DIR__."/../icetea_bot.so"),
+	[
+		["pipe", "r"],
+		["file", "/dev/tty", "w"],
+		["file", "/dev/tty", "w"]
+	],
+	$pipes
+);
+
+
+fwrite($pipes[0], $php);
+fclose($pipes[0]);
+proc_close($proc);
 
 __halt_compiler();
 
